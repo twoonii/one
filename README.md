f495816d14445865df02c720b2552693c0535319
Lucas do Nascimento Diniz <lucas.diniz@nave.com>
2022-06-29 23:45:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TWOONII DESIGN SYSTEM
+
+Repositório destinado a armazenar todo o código do design systema da organização Twoonii.
+
+## Como instalar
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styled from "styled-components";
+
+interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: "small" | "medium" | "large";
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <StyledButton
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+
+  color: white;
+  background-color: #1ea7fd;
+
+  font-size: 14px;
+  padding: 11px 20px;
+`;
